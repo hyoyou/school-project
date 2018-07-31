9920f87fe2dfcbd658c8c537fc157e4dae8b55c2
Heather You <heatherhyou@gmail.com>
2018-07-31 16:34:21 -0400

    work on controller

--- school-project-api/app/controllers/api/locations_controller.rb
+++ school-project-api/app/controllers/api/locations_controller.rb
@@ -1,3 +1,24 @@
 class Api::LocationsController < ApplicationController
-    
+    before_action :set_location, only: [:show, :update, :destroy]
+
+    def index 
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+    def :set_location
+        @location = Location.find_by(id: params[:id])
+    end
+
+    def location_params
+        params.require(:location).permit(:region, :city, :name, :address)
+    end
 end
