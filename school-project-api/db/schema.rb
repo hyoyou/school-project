c9019f6a23dac1161431f71aa83ade59b568ccd4
Heather You <heatherhyou@gmail.com>
2018-07-31 15:21:49 -0400

    add location model and database

--- school-project-api/db/schema.rb
+++ school-project-api/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725075805) do
+ActiveRecord::Schema.define(version: 20180731192012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "region"
+    t.string "city"
+    t.string "name"
+    t.string "address"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
