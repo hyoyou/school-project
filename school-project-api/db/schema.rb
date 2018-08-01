0f3c2a31c3e01449093b9079fbb59a7e786a04d9
vanitanaidu <vanita112003@gmail.com>
2018-07-31 20:14:56 -0400

    created Leaderboard table, model and controller

--- school-project-api/db/schema.rb
+++ school-project-api/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180731192012) do
+ActiveRecord::Schema.define(version: 20180731234021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "leaderboards", force: :cascade do |t|
+    t.integer "rank"
+    t.integer "no_of_checkins"
+    t.integer "user_id"
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "region"
